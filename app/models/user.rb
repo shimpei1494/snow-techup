8d7e849452b4278c6a8b590c7bbc16f3f8180ee6
shimpei1494 <one1218all38@icloud.com>
2022-04-16 16:42:08 +0900

    prefectureのActiveHashモデルの作成とアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  belongs_to :prefecture
 end
