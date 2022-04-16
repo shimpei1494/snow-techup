4de27959967f88ef79981826eb277d5237c7d261
shimpei1494 <one1218all38@icloud.com>
2022-04-16 16:44:34 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :prefecture
+  
+  validates :nickname, presence: ture
+  validates :prefecture_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
+
 end
