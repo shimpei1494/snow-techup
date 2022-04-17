f95408c55f07296dca6740c792a18a70a9e777d7
shimpei1494 <one1218all38@icloud.com>
2022-04-17 23:50:46 +0900

    エラーメッセージを日本語に変換

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角の英字、数字の混合で設定してください'
-  validates :prefecture_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: {other_than: 1, message: "を選択してください"}
 
 end
