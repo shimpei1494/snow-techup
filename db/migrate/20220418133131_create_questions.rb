3e7f060b89beec858db8f2560d512021cad99c5c
shimpei1494 <one1218all38@icloud.com>
2022-04-18 23:00:15 +0900

    questionsテーブルの修正

--- db/migrate/20220418133131_create_questions.rb
+++ db/migrate/20220418133131_create_questions.rb
@@ -2,6 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
       t.text :text, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
