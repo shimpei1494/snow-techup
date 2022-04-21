0ee8222a4c11e3e3bf3ba355b5ab47b8e85738e0
shimpei1494 <one1218all38@icloud.com>
2022-04-21 22:52:35 +0900

    questionを保存するストロングパラメーターの記述

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,10 @@
 class QuestionsController < ApplicationController
   def index
   end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:text, :video).merge(user_id: current_user.id)
+  end
 end
