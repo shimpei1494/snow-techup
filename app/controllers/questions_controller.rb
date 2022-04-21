f974565ebb4274878087bbbb5d4202080acc0ae4
shimpei1494 <one1218all38@icloud.com>
2022-04-21 23:48:54 +0900

    new,createアクションの記述

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,19 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def index
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
