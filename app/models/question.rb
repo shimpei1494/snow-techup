a554eb7eb3e6788a430ab83bbc59d382d65310b0
shimpei1494 <one1218all38@icloud.com>
2022-04-24 16:19:00 +0900

    添付ファイルのタイプバリデーション

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,5 +3,5 @@ class Question < ApplicationRecord
   has_one_attached :video
 
   validates :text, presence: true
-  validates :video, attached_file_size: { maximum: 15.megabytes }
+  validates :video, attached_file_size: { maximum: 15.megabytes }, attached_file_type: { pattern: /^image \// }
 end
