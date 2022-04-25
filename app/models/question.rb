81f6194d6c44a6aa21b2f859f5edaa1d5c131ad3
shimpei1494 <one1218all38@icloud.com>
2022-04-26 06:59:44 +0900

    commentに関するアソシエーションを記述

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   has_one_attached :video
+  has_many :questions
 
   validates :text, presence: true
   validates :video, blob: { content_type: :video, size_range: 1..(15.megabytes) }
