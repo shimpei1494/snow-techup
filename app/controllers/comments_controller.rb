e5368049df0d4213d4048c769bac9f2ea680a463
shimpei1494 <one1218all38@icloud.com>
2022-04-27 05:47:16 +0900

    コメント投稿と表示ができるように

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to question_path(comment.question.id)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, question_id: params[:question_id])
+  end
 end
