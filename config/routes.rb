28685a4f4f11335030a88f594503e708a5abd07a
shimpei1494 <one1218all38@icloud.com>
2022-05-05 22:50:41 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'questions#index'
+  
   resources :questions do
-    resources :comments
+    resources :comments do
+      member do
+        post 'best_answer'
+        post 'best_answer_reset'
+      end
+    end
 
     collection do
       get 'search'
-    end
-    member do
-      post 'best_answer'
-      post 'best_answer_reset'
     end
   end
 end
