8178bd3dfb2c197195101e8a4f3619bd98662727
shimpei1494 <one1218all38@icloud.com>
2022-05-05 22:14:49 +0900

    ベストアンサーのルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   root to: 'questions#index'
   resources :questions do
     resources :comments
+
     collection do
       get 'search'
+    end
+    member do
+      post 'best_answer'
+      post 'best_answer_reset'
     end
   end
 end
